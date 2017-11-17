cf11a4f5bae99b84a828e14de9fde021fa7ae86f
Johannes Schillinger <schillinger@synyx.de>
2017-11-17 14:59:37 +0100

    add functionality to backend to proxy requests to inventory
    includes authentication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-const express = require('express')
+var express = require('express');
+var request = require('request');
+var config = require('../config.json');
 const app = express()
 
-app.use(express.static('src/public'))
+app.use(express.static('src/public'));
 
-app.get('/api', (req, res) => res.send('hello api'))
+
+app.get('/api', (req, res) => {
+    request.get(config.inventory_url + '/hardware/5', {
+            method: 'GET',
+            auth: {
+                'bearer': config.inventory_token,
+            },
+            accept: 'application/json'
+
+
+        },
+        function (err, response, body) {
+            res.send(body);
+
+        })
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
